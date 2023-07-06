ffec34ba95b41a2a01a4d1ea9b6067c99cad854d
Yuri Breion <yuribreion@br.ibm.com>
2023-07-06 18:32:57 -0300

    Customer repository interface

---
+++ springsecuritysession3/src/main/java/com/training/springsecurity/customer/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.training.springsecurity.customer;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findCustomerByEmail(String email);
+}
