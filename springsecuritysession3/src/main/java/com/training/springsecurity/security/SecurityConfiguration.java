24410b24c304c3e376efa1c1abe10ecf19610bb0
Yuri Breion <yuribreion@br.ibm.com>
2023-07-06 18:33:45 -0300

    Customer repository interface

--- springsecuritysession3/src/main/java/com/training/springsecurity/security/SecurityConfiguration.java
+++ springsecuritysession3/src/main/java/com/training/springsecurity/security/SecurityConfiguration.java
@@ -3,13 +3,9 @@ package com.training.springsecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
